575a0193653718f8f9a5e8257ed19b9d291e7eca
Larissa Gomes <lahgomes@LAPTOP-PUQ31PEG.localdomain>
2021-12-17 11:05:16 -0300

    login scss and components

--- src/ui/Button/Button.jsx
+++ src/ui/Button/Button.jsx
@@ -3,11 +3,9 @@ import React from 'react';
 import './Button.scss';
 
 const Button = () => (
-  <button 
-  className="button button--login">
-    Login with Spotify
+  <button className="button">
+    Login 
   </button>
-
 )
 
 export default Button;
