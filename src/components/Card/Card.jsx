53ffc0cdaa7b9c43d1f13cdb13ead7e526914989
Larissa Gomes <lahgomes@LAPTOP-PUQ31PEG.localdomain>
2021-12-22 16:52:56 -0300

    position img card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Card.scss';
+
+const Card = () => (
+  <div className="card">
+    <p className="card__title">The Best of Pop</p>
+
+    <img className="card__img" src="https://i.scdn.co/image/ab67706f00000002a418f22aabf49749111db0c0" alt="" />
+
+  </div>
+   
+);
+
+export default Card;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
-
+import Card from '../../components/Card/Card';
 
 import './Home.scss';
 
 const Home = () => (
+  <>
   <Header />
-  
+  <section className='section'>
+    <h1 className='section__title'> Discover your favorites songs </h1>
+    <Card />
+    {/* <Card />
+    <Card />
+    <Card /> */}
+  </section>   
+  </>  
 );
 
 export default Home;
