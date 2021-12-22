1f1e1a8b5d0b7111ddd2d909dc15d62f1cce22f2
Larissa Gomes <lahgomes@LAPTOP-PUQ31PEG.localdomain>
2021-12-22 18:32:58 -0300

    cards mobile and desktop

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 import './Card.scss';
 
-const Card = () => (
-  <div className="card">
-    <p className="card__title">The Best of Pop</p>
-
-    <img className="card__img" src="https://i.scdn.co/image/ab67706f00000002a418f22aabf49749111db0c0" alt="" />
-
-  </div>
-   
+const Card = ({ title, src, color }) => (
+  <div className="card" style={{ backgroundColor: `${color}` }}>
+    <h2 className="card__title">
+      { title }
+    </h2>
+    <img className="card__img" src={src} alt={title} />
+  </div>  
 );
 
 export default Card;
