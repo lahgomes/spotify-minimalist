dc83d1c1ba019ece42c15656d08081ef1f06cad6
Larissa Gomes <lahgomes@LAPTOP-PUQ31PEG.localdomain>
2022-01-03 09:29:21 -0300

    cards

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,57 +4,67 @@ import Card from '../../components/Card/Card';
 
 import './Home.scss';
 
-const Home = () => (
-  <>
-  <Header />
-  <main className='main'>
-    <h1 className='main__title'>
-      Discover your favorites songs
-    </h1>
+const Home = () => {
 
-    <section className="wrapper-category">
-      <Card title="The Best of Pop" src="https://i.scdn.co/image/ab67706f00000002a418f22aabf49749111db0c0" color="#27856a" />
-      <Card title="Best of 2021" src="https://i.scdn.co/image/ab67706f000000023ba7e5967c8a2ba24c37e3dc" color="#3d91f4"/>
-      <Card title="Classic Rock" src="https://i.scdn.co/image/ab67706f000000026705dfb8f5445fa4fdddf374" color="#ffa42b" />
-      <Card title="Indie" src="https://i.scdn.co/image/ab67616d00001e0296620e04b099b836684a3312" color="#f15e6c" />
-      <Card title="The Best of Pop" src="https://i.scdn.co/image/ab67706f00000002a418f22aabf49749111db0c0" color="#27856a" />
-      <Card title="Best of 2021" src="https://i.scdn.co/image/ab67706f000000023ba7e5967c8a2ba24c37e3dc" color="#3d91f4"/>
-      <Card title="Classic Rock" src="https://i.scdn.co/image/ab67706f000000026705dfb8f5445fa4fdddf374" color="#ffa42b" />
-      <Card title="Indie" src="https://i.scdn.co/image/ab67616d00001e0296620e04b099b836684a3312" color="#f15e6c" />
-    </section>
+  const categories = [
+    {id: 'favorites', title: 'Discover your favorites songs'},
+    {id: 'podcasts', title: 'Podcasts'},
+    {id: 'browse', title: 'Browse all'}
+  ];
 
-    <h1 className='main__title'>
-      Podcasts
-    </h1>
-    
-    <section className="wrapper-category">
-      <Card title="Modus Operandi" src="https://i.scdn.co/image/ab67656300005f1f58c7a48f09adfdd65127304b" color="#1e3264" />
-      <Card title="Não Inviabilize" src="https://i.scdn.co/image/9dbecec261183fa7479bf347d2cd9c9fc025ae6a" color="#b49bc8"/>
-      <Card title="Wanda Experimenta" src="https://i.scdn.co/image/ab67706c0000da8409b724bc0ce12a9d25978739" color="#f15e6c"/>
-      <Card title="Estamos bem?" src="https://i.scdn.co/image/ab67656300005f1feda422b90c8173a72b48947c" color="#0d72ea"/>
-      <Card title="Modus Operandi" src="https://i.scdn.co/image/ab67656300005f1f58c7a48f09adfdd65127304b" color="#1e3264" />
-      <Card title="Não Inviabilize" src="https://i.scdn.co/image/9dbecec261183fa7479bf347d2cd9c9fc025ae6a" color="#b49bc8"/>
-      <Card title="Wanda Experimenta" src="https://i.scdn.co/image/ab67706c0000da8409b724bc0ce12a9d25978739" color="#f15e6c"/>
-      <Card title="Estamos bem?" src="https://i.scdn.co/image/ab67656300005f1feda422b90c8173a72b48947c" color="#0d72ea"/>
-    </section>
+  const items = [
+    {title:'The Best of Pop', url:'https://i.scdn.co/image/ab67706f00000002a418f22aabf49749111db0c0', color:'#27856a',category:'favorites'},
+    {title:'Best of 2021', url:'https://i.scdn.co/image/ab67706f000000023ba7e5967c8a2ba24c37e3dc', color:'#3d91f4',category:'favorites'},
+    {title:'Classic Rock', url:'https://i.scdn.co/image/ab67706f000000026705dfb8f5445fa4fdddf374', color:'#ffa42b',category:'favorites'},
+    {title:'Indie', url:'https://i.scdn.co/image/ab67616d00001e0296620e04b099b836684a3312', color:'#f15e6c',category:'favorites'},
+    {title:'The Best of Pop', url:'https://i.scdn.co/image/ab67706f00000002a418f22aabf49749111db0c0', color:'#27856a',category:'favorites'},
+    {title:'Best of 2021', url:'https://i.scdn.co/image/ab67706f000000023ba7e5967c8a2ba24c37e3dc', color:'#3d91f4',category:'favorites'},
+    {title:'Classic Rock', url:'https://i.scdn.co/image/ab67706f000000026705dfb8f5445fa4fdddf374', color:'#ffa42b',category:'favorites'},
+    {title:'Indie', url:'https://i.scdn.co/image/ab67616d00001e0296620e04b099b836684a3312', color:'#f15e6c',category:'favorites'},
 
-    <h1 className='main__title'>
-      Browse all
-    </h1>
+    {title:'Modus Operandi', url:'https://i.scdn.co/image/ab67656300005f1f58c7a48f09adfdd65127304b', color:'#1e3264',category:'podcasts'},
+    {title:'Não Inviabilize', url:'https://i.scdn.co/image/9dbecec261183fa7479bf347d2cd9c9fc025ae6a', color:'#b49bc8',category:'podcasts'},
+    {title:'Wanda Experimenta', url:'https://i.scdn.co/image/ab67706c0000da8409b724bc0ce12a9d25978739', color:'#f15e6c',category:'podcasts'},
+    {title:'Estamos bem?', url:'https://i.scdn.co/image/ab67656300005f1feda422b90c8173a72b48947c', color:'#0d72ea',category:'podcasts'},
+    {title:'Modus Operandi', url:'https://i.scdn.co/image/ab67656300005f1f58c7a48f09adfdd65127304b', color:'#1e3264',category:'podcasts'},
+    {title:'Não Inviabilize', url:'https://i.scdn.co/image/9dbecec261183fa7479bf347d2cd9c9fc025ae6a', color:'#b49bc8',category:'podcasts'},
+    {title:'Wanda Experimenta', url:'https://i.scdn.co/image/ab67706c0000da8409b724bc0ce12a9d25978739', color:'#f15e6c',category:'podcasts'},
+    {title:'Estamos bem?', url:'https://i.scdn.co/image/ab67656300005f1feda422b90c8173a72b48947c', color:'#0d72ea',category:'podcasts'},
 
-    <section className="wrapper-category">
-      <Card title="The Best of Pop" src="https://i.scdn.co/image/ab67706f00000002a418f22aabf49749111db0c0" color="#27856a" />
-      <Card title="Best of 2021" src="https://i.scdn.co/image/ab67706f000000023ba7e5967c8a2ba24c37e3dc" color="#3d91f4"/>
-      <Card title="Classic Rock" src="https://i.scdn.co/image/ab67706f000000026705dfb8f5445fa4fdddf374" color="#ffa42b" />
-      <Card title="Indie" src="https://i.scdn.co/image/ab67616d00001e0296620e04b099b836684a3312" color="#f15e6c" />
-      <Card title="The Best of Pop" src="https://i.scdn.co/image/ab67706f00000002a418f22aabf49749111db0c0" color="#27856a" />
-      <Card title="Best of 2021" src="https://i.scdn.co/image/ab67706f000000023ba7e5967c8a2ba24c37e3dc" color="#3d91f4"/>
-      <Card title="Classic Rock" src="https://i.scdn.co/image/ab67706f000000026705dfb8f5445fa4fdddf374" color="#ffa42b" />
-      <Card title="Indie" src="https://i.scdn.co/image/ab67616d00001e0296620e04b099b836684a3312" color="#f15e6c" />
-    </section>
+    {title:'The Best of Pop', url:'https://i.scdn.co/image/ab67706f00000002a418f22aabf49749111db0c0', color:'#27856a',category:'browse'},
+    {title:'Best of 2021', url:'https://i.scdn.co/image/ab67706f000000023ba7e5967c8a2ba24c37e3dc', color:'#3d91f4',category:'browse'},
+    {title:'Classic Rock', url:'https://i.scdn.co/image/ab67706f000000026705dfb8f5445fa4fdddf374', color:'#ffa42b',category:'browse'},
+    {title:'Indie', url:'https://i.scdn.co/image/ab67616d00001e0296620e04b099b836684a3312', color:'#f15e6c',category:'browse'},
+    {title:'The Best of Pop', url:'https://i.scdn.co/image/ab67706f00000002a418f22aabf49749111db0c0', color:'#27856a',category:'browse'},
+    {title:'Best of 2021', url:'https://i.scdn.co/image/ab67706f000000023ba7e5967c8a2ba24c37e3dc', color:'#3d91f4',category:'browse'},
+    {title:'Classic Rock', url:'https://i.scdn.co/image/ab67706f000000026705dfb8f5445fa4fdddf374', color:'#ffa42b',category:'browse'},
+    {title:'Indie', url:'https://i.scdn.co/image/ab67616d00001e0296620e04b099b836684a3312', color:'#f15e6c',category:'browse'}    
+  ];
 
-  </main>   
-  </>  
-);
+  return (
+    <>
+      <main className='main'>
+        <Header />
+      </main> 
+      {
+        categories.map((category)=>(
+          <section key={category.id}>
+            <h2 className='main__title'>{category.title}</h2>
+            <ul className='wrapper-category'>
+              {items.filter((item) => item.category === category.id).map((item, index) => (
+                <li key={index}>
+                  <Card 
+                    title={item.title} 
+                    src={item.url} 
+                    color={item.color} 
+                  />
+                </li>
+              ))}
+            </ul>
+          </section>
+        ))
+      }      
+    </>  
+  )};
 
 export default Home;
